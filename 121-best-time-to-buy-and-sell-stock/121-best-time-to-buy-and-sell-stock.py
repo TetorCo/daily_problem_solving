fd4483ae495d1cdbd98b1851e9865f1f2d04c393
김태범 <76984534+TetorCo@users.noreply.github.com>
2022-02-01 17:12:46 +0900

    Time: 1783 ms (16.45%), Space: 25 MB (80.69%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit, min_prices = 0, float('inf')
+        max_profit, min_prices = 0, float('inf')  # float('inf') => 양의 무한대
         
         for p in prices:
-            min_prices = min(p, min_prices)
+            min_prices = min(p, min_prices)  # 양의 무한대 값과 prices 값을 비교
             profit = p-min_prices
             max_profit = max(profit, max_profit)
         return max_profit
