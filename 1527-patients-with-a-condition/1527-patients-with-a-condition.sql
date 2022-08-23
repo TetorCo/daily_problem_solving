eccae57f517bffa69e2525f2be03719e345b4cbf
김태범 <76984534+TetorCo@users.noreply.github.com>
2022-08-24 00:46:35 +0900

    Time: 505 ms (38.94%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Patients
-WHERE conditions REGEXP '\\bDIAB1'
+WHERE conditions REGEXP '\\bDIAB1' ;
 /*WHERE conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%' ;*/
