32f3ed43815d8499beec36e425d8d940557b4ca8
김태범 <76984534+TetorCo@users.noreply.github.com>
2022-08-31 00:05:07 +0900

    Time: 667 ms (45.94%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT e1.employee_id
+/* SELECT e1.employee_id
 FROM 
     (SELECT *
     FROM Employees
@@ -10,5 +10,20 @@ FROM
     FROM Employees
     RIGHT JOIN Salaries
     USING (employee_id)) as e1
+WHERE e1.name IS NULL OR e1.salary IS NULL
+ORDER BY e1.employee_id ; */
+
+SELECT e1.employee_id
+FROM
+    (SELECT e.employee_id, e.name, s.salary
+     FROM Employees as e
+     LEFT JOIN Salaries as s
+     ON e.employee_id = s.employee_id
+     UNION
+     SELECT s.employee_id, e.name, s.salary
+     FROM Employees as e
+     RIGHT JOIN Salaries as s
+     ON e.employee_id = s.employee_id
+    ) as e1
 WHERE e1.name IS NULL OR e1.salary IS NULL
 ORDER BY e1.employee_id ;
