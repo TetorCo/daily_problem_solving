ab563af1c8ea915fcb76c78e0b5f1d8bc7403682
김태범 <76984534+TetorCo@users.noreply.github.com>
2022-09-05 00:50:18 +0900

    Time: 513 ms (10.37%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -3,8 +3,12 @@
 FROM Employee
 WHERE salary < (SELECT max(salary) FROM Employee) ; */
 
-SELECT IFNULL(
+/*SELECT IFNULL(
     Null,
     (SELECT DISTINCT Salary FROM Employee
     ORDER BY Salary DESC LIMIT 1 OFFSET 1)
-) as 'SecondHighestSalary'
+) as 'SecondHighestSalary' ; */
+
+SELECT MAX(e2.Salary) as 'SecondHighestSalary'
+FROM Employee e1, Employee e2
+WHERE e1.Salary > e2.Salary ;
