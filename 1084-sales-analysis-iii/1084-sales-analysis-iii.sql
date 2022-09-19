748f3b52e7113235bdbc8b9f43f14eb8a32d4348
김태범 <76984534+TetorCo@users.noreply.github.com>
2022-09-19 22:49:17 +0900

    Time: 2051 ms (11.53%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -3,5 +3,5 @@ SELECT p.product_id, p.product_name
 FROM Product as p
 LEFT JOIN Sales as s
 ON p.product_id = s.product_id
-GROUP BY s.product_id
+GROUP BY p.product_id
 HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31' ;
