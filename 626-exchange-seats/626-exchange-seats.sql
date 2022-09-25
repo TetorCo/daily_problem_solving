37ffb290d02cc62f770cd5cdb36cfde01285f419
김태범 <76984534+TetorCo@users.noreply.github.com>
2022-09-25 16:38:34 +0900

    Time: 441 ms (33.62%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT
-    CASE 
-        WHEN id%2 = 0 THEN id-1
-        WHEN id%2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+    CASE
+        WHEN id%2=0 THEN id-1
+        WHEN id%2=1 AND id = (SELECT MAX(id) FROM Seat) THEN id
         ELSE id+1
     END id,
     student
